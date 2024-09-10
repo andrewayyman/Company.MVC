d6682c6c0b95c30590adf65149f18581b1f0ebd2
Andrew Ayman <andrewayman1000@gmail.com>
2024-09-10 23:23:13 +0300

    Add project files.

---
+++ Company.Route.DAL/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Company.Route.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Route.DAL.Data.Contexts
+{
+    internal class AppDbContext : DbContext 
+    {
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+        {
+            optionsBuilder.UseSqlServer(" Server=WILDRABBIT; Database=CompanyMVC; Trusted_Connection=True; TrustServerCertificate = True " );
+        }
+
+        // to use config class
+        protected override void OnModelCreating( ModelBuilder modelBuilder )
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
