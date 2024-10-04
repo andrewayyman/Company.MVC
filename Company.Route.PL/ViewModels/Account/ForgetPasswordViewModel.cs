6dc6acb5cf7938c096dff4d325a434d25dc7ff10
andrew ayman <andrewayman1000@gmail.com>
2024-10-04 22:40:55 +0300

    ForgetPassword & SendEmail , EmailSettings_using_smtp

---
+++ Company.Route.PL/ViewModels/Account/ForgetPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Route.PL.ViewModels.Account
+{
+	public class ForgetPasswordViewModel
+	{
+		[Required(ErrorMessage ="Email is Requierd")]
+		[EmailAddress (ErrorMessage ="Invalid Email Address")]
+        public string Email { get; set; }
+    }
+}
---
+++ Company.Route.PL/ViewModels/Account/ResetPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Route.PL.ViewModels.Account
+{
+	public class ResetPasswordViewModel
+	{
+		[Required(ErrorMessage = "Password is required")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Compare(nameof(Password), ErrorMessage = "Password doesn't match!!")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+	}
+}
